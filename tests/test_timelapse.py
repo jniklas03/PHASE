b542efb2739e1ba8c11325ba1e07e922be6a5ede
jniklas03 <jakubniklas03@gmail.com>
2025-11-18 10:02:35 +0100

    renamed test_main to test_timelapse

--- tests/test_timelapse.py
+++ tests/test_timelapse.py
@@ -1,8 +1,8 @@
 from phase import timelapse_pipeline
-from test_paths import *
+from test_paths import TIMELAPSE, TIMELAPSE_ACEGLU, SAVE_PATH
 
 timelapse_pipeline(
-    source=TIMELAPSE, 
+    source=TIMELAPSE_ACEGLU, 
     save_intermediates=True,
     save_path=SAVE_PATH, 
     plot=True
