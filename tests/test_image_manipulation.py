0a41fb25d054fe89d1a415a3ff1e5868556c17f5
jniklas03 <jakubniklas03@gmail.com>
2025-11-18 10:04:55 +0100

    added a file_name variable

--- tests/test_image_manipulation.py
+++ tests/test_image_manipulation.py
@@ -1,11 +1,15 @@
 import phase
-from test_paths import *
+from test_paths import RAW1, RAW2, RAW3, RAW4, RAW5, RAW6, RAW7, RAW8, SAVE_PATH
 import os
 
+SOURCE = RAW7
+
+file_name = os.path.splitext(os.path.basename(SOURCE))[0]
+
 dishes, masks, coordinates, metadata = phase.detect_dishes(
-    source=RAW5,
+    source=SOURCE,
     save_path=SAVE_PATH,
-    file_name=os.path.splitext(os.path.basename(RAW5))[0],
+    file_name=file_name,
     debug=False,
     save=False
 )
@@ -19,7 +23,7 @@ for idx, (dish, mask) in enumerate(zip(dishes, masks)):
         area_filter=False,
         save=True,
         save_path=SAVE_PATH,
-        file_name=os.path.splitext(os.path.basename(RAW5))[0],
+        file_name=file_name,
         idx=idx+1
     )
     preprocessed.append(preprocessed_img)
@@ -30,6 +34,6 @@ for idx, preprocessed_img in enumerate(preprocessed):
         raw_img=dishes[idx],
         save=True,
         save_path=SAVE_PATH,
-        file_name=os.path.splitext(os.path.basename(RAW5))[0],
-        idx=idx
+        file_name=file_name,
+        idx=idx+1
     )
