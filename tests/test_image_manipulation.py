c2243eac959b208dd576415c5a646633d1dc5b00
jniklas03 <jakubniklas03@gmail.com>
2025-12-08 20:13:27 +0100

    playing around with the tests

--- tests/test_image_manipulation.py
+++ tests/test_image_manipulation.py
@@ -2,7 +2,7 @@ import phase
 from test_paths import RAW1, RAW2, RAW3, RAW4, RAW5, RAW6, RAW7, RAW8, SAVE_PATH
 import os
 
-SOURCE = RAW7
+SOURCE = RAW1
 
 file_name = os.path.splitext(os.path.basename(SOURCE))[0]
 
@@ -10,30 +10,30 @@ dishes, masks, coordinates, metadata = phase.detect_dishes(
     source=SOURCE,
     save_path=SAVE_PATH,
     file_name=file_name,
-    debug=False,
-    save=False
+    debug=True,
+    save=True
 )
 
-preprocessed = []
+# preprocessed = []
 
-for idx, (dish, mask) in enumerate(zip(dishes, masks)):
-    preprocessed_img = phase.preprocess(
-        source=dish,
-        mask=mask,
-        area_filter=False,
-        save=True,
-        save_path=SAVE_PATH,
-        file_name=file_name,
-        idx=idx+1
-    )
-    preprocessed.append(preprocessed_img)
+# for idx, (dish, mask) in enumerate(zip(dishes, masks)):
+#     preprocessed_img = phase.preprocess(
+#         source=dish,
+#         mask=mask,
+#         area_filter=False,
+#         save=True,
+#         save_path=SAVE_PATH,
+#         file_name=file_name,
+#         idx=idx+1
+#     )
+#     preprocessed.append(preprocessed_img)
 
-for idx, preprocessed_img in enumerate(preprocessed):
-    phase.detect_colonies(
-        source=preprocessed_img,
-        raw_img=dishes[idx],
-        save=True,
-        save_path=SAVE_PATH,
-        file_name=file_name,
-        idx=idx+1
-    )
+# for idx, preprocessed_img in enumerate(preprocessed):
+#     phase.detect_colonies(
+#         source=preprocessed_img,
+#         raw_img=dishes[idx],
+#         save=True,
+#         save_path=SAVE_PATH,
+#         file_name=file_name,
+#         idx=idx+1
+#     )
--- tests/test_timelapse.py
+++ tests/test_timelapse.py
@@ -1,11 +1,14 @@
 from phase import timelapse_pipeline
 from test_paths import TIMELAPSE, TIMELAPSE_ACEGLU, SAVE_PATH
 
+ANTIBIOTIKA = r"C:\Users\jakub\Documents\Bachelorarbeit\Resources\Sources\Antibiotika"
+
 timelapse_pipeline(
-    source=TIMELAPSE_ACEGLU, 
+    source=ANTIBIOTIKA, 
     save_intermediates=True,
     save_path=SAVE_PATH, 
     plot=True,
-    use_masks=False,
-    fine_buffer=0
+    use_masks=True,
+    fine_buffer=2,
+    n_to_stack=3
 )
