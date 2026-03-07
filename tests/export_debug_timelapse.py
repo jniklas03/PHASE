63290056382c284f1904ad6fbd4544808630b993
jniklas03 <jakubniklas03@gmail.com>
2026-03-07 10:02:13 +0100

    naming changes

--- tests/export_debug_timelapse.py
+++ tests/export_debug_timelapse.py
@@ -5,7 +5,7 @@ DIR = SAVE_PATH
 
 if __name__ == "__main__":
     ts = Timeseries.from_directory("test", TIMELAPSE)
-    ts.populate_timeseries()
+    ts.generate_dishes_timeseries()
     ts.preprocess_timeseries()
     ts.detect_timeseries()
     ts.plot_counts(DIR)
