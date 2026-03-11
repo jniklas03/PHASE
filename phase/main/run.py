53e6f0058923decbcaf1c4b0e1cc6ed583d5278b
jniklas03 <jakubniklas03@gmail.com>
2026-03-11 11:55:16 +0100

    changed run to finish processing one timeseries before starting another

--- phase/main/run.py
+++ phase/main/run.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass, field
 from pathlib import Path
 from tqdm import tqdm
-from concurrent.futures import ThreadPoolExecutor
-
+import pandas as pd
+import os
 
 from .timeseries import Timeseries
 from .colony import CostFunction
@@ -10,7 +10,7 @@ from .colony import CostFunction
 @dataclass
 class Run:
     name: str
-    timeseries_list: list[Timeseries] = field(default_factory=list)
+    stats: list = field(default_factory=list)
 
     @classmethod
     def from_directory(
@@ -20,63 +20,44 @@ class Run:
         load_timeseries=True
         ):
         run = cls(name=name)
-        run.load_run(directory, load_timeseries=load_timeseries)
+        run.execute_run(directory, load_timeseries=load_timeseries)
         return run
 
-    def load_run(
+    def execute_run(
             self,
             directory: str | Path,
-            load_timeseries=True,
+            use_stencil=True,
+            use_bg_mask=True,
+            use_fg_mask=True,
+            use_area_filter=False,
+            detection_threshold=0.99,
+            distance_threshold=10,
+            min_lost_radius=2,
+            cost_function: CostFunction = CostFunction.IOU_CIRCLE,
+            verbosity=0,
+            save_path: str | Path = ""
         ):
         directory = Path(directory)
 
         if not directory.is_dir():
             raise TypeError("directory must be a string of directory path (str or Path).")
         
-        for item in tqdm(sorted(directory.iterdir()), desc="Loading run"):
+        for item in tqdm(sorted(directory.iterdir()), desc="Executing run"):
             if item.is_dir():
-                timeseries = Timeseries(
-                    name=item.name
-                )
-                
-                if load_timeseries:
-                    timeseries.load_timeseries(item)
+                save_dir = (save_path / item.name)
+                save_dir.mkdir(parents=True, exist_ok=True)
+                ts = Timeseries.from_directory(f"{item.name}", item)
+                ts.generate_dishes_timeseries(use_stencil=use_stencil)
+                ts.preprocess_timeseries(use_bg_mask=use_bg_mask, use_fg_mask=use_fg_mask, use_area_filter=use_area_filter)
+                ts.detect_timeseries(detection_threshold=detection_threshold, distance_threshold=distance_threshold, min_lost_radius=min_lost_radius, cost_function=cost_function, verbosity=verbosity)
 
-                self.timeseries_list.append(timeseries)
+                stats = ts.export_stats()
+                pd.DataFrame(stats).to_csv(os.path.join(save_dir, f"{item.name}_stats.csv"))
+                self.stats.extend(stats)
 
-    def populate_run(self, use_stencil=True):
-        with ThreadPoolExecutor() as ex:
-            list(tqdm(
-                ex.map(lambda ts: ts.generate_dishes_timeseries(use_stencil), self.timeseries_list),
-                total=len(self.timeseries_list),
-                desc="Populating run"
-            ))
+                ts.export_images(save_path=save_dir)
+                ts.plot_counts(save_path=save_dir)
+                ts.delete()
+                del ts
+        
 
-    def preprocess_run(
-            self,
-            use_bg_mask = True,
-            use_fg_mask = True,
-            use_area_filter = False
-    ):
-        for ts in tqdm(self.timeseries_list, desc="Preprocessing timeseries"):
-            ts.preprocess_timeseries(use_bg_mask, use_fg_mask, use_area_filter)
-
-    def detect_run(
-            self,
-            threshold = 0.9,
-            verbosity = 0,
-            cost_function: CostFunction = CostFunction.IOU_CIRCLE,
-            min_lost_radius = 1
-    ):
-        for ts in tqdm(self.timeseries_list, desc="Detecting timeseries"):
-            ts.detect_timeseries(threshold, verbosity, cost_function, min_lost_radius)
-
-    def export_run(
-            self,
-            save_path: str | Path = ""
-    ):
-        save_path = Path(save_path)
-        for ts in tqdm(self.timeseries_list, desc="Exporting run"):
-            ts_path = save_path / ts.name
-            ts_path.mkdir(parents=True, exist_ok=True)
-            ts.export_images(ts_path)
