811c74386e5205abbe51637c48383208aa775ec5
jniklas03 <jakubniklas03@gmail.com>
2026-03-15 12:57:15 +0100

    spelling error

--- tests/data.py
+++ tests/data.py
@@ -4,6 +4,6 @@ THIS_DIR = Path(__file__).resolve().parent
 DATA_DIR = THIS_DIR.parent / "data"
 
 TIMELAPSE = DATA_DIR / "Timelapse" / "T1"
-SYNTHETIC = DATA_DIR / "Syntetic"
+SYNTHETIC = DATA_DIR / "Synthetic"
 
 SAVE_PATH = DATA_DIR / "Saved"
