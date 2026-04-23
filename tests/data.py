6e6d82c15aa19483c187ac76a36effe3512544b6
jniklas03 <jakubniklas03@gmail.com>
2026-04-23 08:45:30 +0200

    Deleted tests

--- tests/data.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-THIS_DIR = Path(__file__).resolve().parent
-DATA_DIR = THIS_DIR.parent / "data"
-
-TIMELAPSE = DATA_DIR / "Timelapse" / "T1"
-SYNTHETIC = DATA_DIR / "Synthetic"
-
-SAVE_PATH = DATA_DIR / "Saved"
